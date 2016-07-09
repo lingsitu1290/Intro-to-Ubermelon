e8bad770f4607bfd0d0c133ac0d05ec4a445e9ca
Ling Situ <lingsitu1290@gmail.com>
2016-07-09 09:04:53 -0700

    Updated conditionals

--- underpaid-customers/accounting.py
+++ underpaid-customers/accounting.py
@@ -1,19 +1,22 @@
 melon_cost = 1.00
 
-def underpaid_customers(name, melons, paid):
+def customers_pay(name, melons, paid):
     """ Determine if any client's paid amount is different from expected amount. """
 
     # to find the expected amount by multiplying melons by melon_cost
     expected = melons * melon_cost
-    # print only the expected and the paid amount is different 
-    if expected != paid:
-        print name, "paid {:.2f}, expected {:.2f}".format(paid, expected)
+    # if customer underpaid 
+    if expected > paid:
+        print name, "paid ${:.2f}, expected ${:.2f} which is less than expected.".format(paid, expected)
+    # if customer overpaid
+    elif expected < paid: 
+        print name, "paid ${:.2f}, expected ${:.2f} which is more than expected.".format(paid, expected)
 
 #call functions on given people
-underpaid_customers('Joe', 5, 5.00)
-underpaid_customers('Frank', 6, 6.00)
-underpaid_customers('Sally', 3, 3.00)
-underpaid_customers('Sean', 9, 9.50)
-underpaid_customers('David', 4, 4.00)
-underpaid_customers('Ashley', 3, 2.00)
+customers_pay('Joe', 5, 5.00)
+customers_pay('Frank', 6, 6.00)
+customers_pay('Sally', 3, 3.00)
+customers_pay('Sean', 9, 9.50)
+customers_pay('David', 4, 4.00)
+customers_pay('Ashley', 3, 2.00)
 
